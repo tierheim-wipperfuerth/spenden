e9dd462e74015c6bfe75d76a9e33bb27cc4490a6
Jan-Markus Langer <janmarkuslanger10121994@gmail.com>
2024-08-17 21:04:13 +0200

    Add imprint and datasecure

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,10 +7,6 @@ const router = createRouter({
     return { top: 0 }
   },
   history: createWebHistory(import.meta.env.BASE_URL),
-  scrollBehavior(to, from, savedPosition) {
-    // always scroll to top
-    return { top: 0 }
-  },
   routes: [
     {
       path: '/',
